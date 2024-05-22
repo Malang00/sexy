b6f45c0e553610ac3f493706d50142ad593833e1
Malang00 <91902331+Malang00@users.noreply.github.com>
2024-05-22 13:32:33 +0530

    Update pro.py

--- plugins/pro.py
+++ plugins/pro.py
@@ -56,8 +56,8 @@ async def Pro(bot: AFK, m: Message):
             name = BOT.parse_name(nameLinks[i][0])
             link = nameLinks[i][1]
             wxh = get_link_atributes().get_height_width(link=link, Q=quality)
-            caption_name = f"**{str(i+1).zfill(3)}.** - {name} {wxh}"
-            file_name = f"{str(i+1).zfill(3)}. - {BOT.short_name(name)} {wxh}"
+            caption_name = f"**{str(i+1).zfill(3)}.** {name} {wxh}"
+            file_name = f"{str(i+1).zfill(3)}. {BOT.short_name(name)} {wxh}"
             print(caption_name, link)
 
             Show = await bot.send_message(
@@ -76,13 +76,13 @@ async def Pro(bot: AFK, m: Message):
 
             if os.path.isfile(dl_file) is not None:
                 if dl_file.endswith(".mp4"):
-                    cap = f"{caption_name}.mp4\n\n<b>𝗕𝗮𝘁𝗰𝗵 𝗡𝗮𝗺𝗲 : </b>{caption}\n\n<b>𝗘𝘅𝘁𝗿𝗮𝗰𝘁𝗲𝗱 𝗯𝘆 ➤ </b> **{userr}**"
+                    cap = f"{caption_name}.mp4\n\n<b>**Batch Name :** </b>{caption}\n\n<b>**Download By :** </b> **{userr}**"
                     UL = Upload_to_Tg(bot=bot, m=m, file_path=dl_file, name=caption_name,
                                       Thumb=Thumb, path=sPath, show_msg=Show, caption=cap)
                     await UL.upload_video()
                 else:
                     ext = dl_file.split(".")[-1]
-                    cap = f"{caption_name}.{ext}\n\n<b>𝗕𝗮𝘁𝗰𝗵 𝗡𝗮𝗺𝗲 : </b>{caption}\n\n<b>𝗘𝘅𝘁𝗿𝗮𝗰𝘁𝗲𝗱 𝗯𝘆 ➤ </b> **{userr}**"
+                    cap = f"{caption_name}. {ext}\n\n<b>**Batch Name :** </b>{caption}\n\n<b>**Downloaded By :** </b> **{userr}**"
                     UL = Upload_to_Tg(bot=bot, m=m, file_path=dl_file, name=caption_name,
                                       Thumb=Thumb, path=sPath, show_msg=Show, caption=cap)
                     await UL.upload_doc()
